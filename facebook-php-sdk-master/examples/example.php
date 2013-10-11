6bd57dad75f7009ecda3315b7918eddf63655569
hanfandan <fuwaxiuxiu@gmail.com>
2013-10-10 21:35:16 -0400

    Update example.php

--- facebook-php-sdk-master/examples/example.php
+++ facebook-php-sdk-master/examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '215611218607442',
+  'secret' => '6765f0d0646459dafd11b2c93929a1b6',
 ));
 
 // Get User ID
@@ -44,6 +44,7 @@ if ($user) {
 
 // Login or logout url will be needed depending on current user state.
 if ($user) {
+  
   $logoutUrl = $facebook->getLogoutUrl();
 } else {
   $loginUrl = $facebook->getLoginUrl();
