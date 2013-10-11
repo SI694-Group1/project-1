8336b8a22ae2e61f6f1f378af8e5c5ca2f759b61
rishabhgandotra <rishabhgandotra@gmail.com>
2013-10-11 12:11:54 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$con=mysqli_connect("localhost","root","","facebook");
+$con=mysqli_connect("localhost","root","","facebookapi_project1");
 
 // Check connection
 if (mysqli_connect_errno($con))
