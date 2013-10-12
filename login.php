b7aef23ce8123b42f30724360660b8ff8adab541
SherlocQ <chengchangqian@gmail.com>
2013-10-12 15:04:37 -0400

    Update login.php

--- login.php
+++ login.php
@@ -34,24 +34,10 @@ if ($user) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
   <title>Login</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- Loading Bootstrap -->
-  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-
-  <!-- Loading Flat UI -->
-  <link href="css/flat-ui.css" rel="stylesheet">
-  <link rel="shortcut icon" href="images/favicon.ico">
-
-  <!-- Loading customized UI -->
-  <link href="css/style.css" rel="stylesheet">
-
-  <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
-  <!--[if lt IE 9]>
-  <script src="js/html5shiv.js"></script>
-  <![endif]-->
+  <?php
+    include 'header.php'
+  ?>
 </head>
 <body>
   <div class="page login-page">
@@ -62,12 +48,12 @@ if ($user) {
       <div class="row">
         <div class="col-md-2">
           <?php 
-            if($user) {
-              echo '<img src="https://graph.facebook.com/'.$user.'/picture?width=100&height=100">';
-              echo '<h5>Hi, '.$user_name.'</h5>';
-            } else {
-              echo '<h5>Welcome to Vote App!</h5>';
-            }    
+          if($user) {
+            echo '<img src="https://graph.facebook.com/'.$user.'/picture?width=100&height=100">';
+            echo '<h5>Hi, '.$user_name.'</h5>';
+          } else {
+            echo '<h5>Welcome to Vote App!</h5>';
+          }    
           ?>
         </div>
         <div class="col-md-10">
@@ -83,22 +69,11 @@ if ($user) {
     </div>
   </div>
 </div>
+</div>
 
-  <!-- Load JS here for greater good -->
-  <script src="js/jquery-1.8.3.min.js"></script>
-  <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
-  <script src="js/jquery.ui.touch-punch.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/bootstrap-select.js"></script>
-  <script src="js/bootstrap-switch.js"></script>
-  <script src="js/flatui-checkbox.js"></script>
-  <script src="js/flatui-radio.js"></script>
-  <script src="js/jquery.tagsinput.js"></script>
-  <script src="js/jquery.placeholder.js"></script>
-  <script>
-      $(document).ready(function() {
-          //Do something
-        });
-      </script>
+<?php
+  include 'footer.php'
+?>
+
 </body>
 </html>
