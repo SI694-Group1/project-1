6eb35a0fb80efd4daa144ce7f382251c81be8e78
SherlocQ <chengchangqian@gmail.com>
2013-10-12 15:02:26 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,23 +1,20 @@
-<head>
-  <meta charset="utf-8">
-  <title>Friend</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Loading Bootstrap -->
-  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<!-- Loading Bootstrap -->
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 
-  <!-- Loading Flat UI -->
-  <link href="css/flat-ui.css" rel="stylesheet">
-  <link href="images/favicon.ico" rel="shortcut icon">
-  
-  <!-- loading libraries style sheets -->
-  <link href="css/jquery.ui.autocomplete.css" rel="stylesheet"/>
+<!-- Loading Flat UI -->
+<link href="css/flat-ui.css" rel="stylesheet">
+<link href="images/favicon.ico" rel="shortcut icon">
 
-  <!-- Loading customized UI -->
-  <link href="css/style.css" rel="stylesheet">
+<!-- loading libraries style sheets -->
+<link href="css/jquery.ui.autocomplete.css" rel="stylesheet"/>
 
-  <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
-  <!--[if lt IE 9]>
-  <script src="js/html5shiv.js"></script>
-  <![endif]-->
-</head>
+<!-- Loading customized UI -->
+<link href="css/style.css" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<![endif]-->
