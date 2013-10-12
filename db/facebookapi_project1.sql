2be7d3293e8b8b887c74e4387e94e5eea0b6ec51
Rishabh Gandotra <rishabhgandotra@gmail.com>
2013-10-12 15:46:44 -0400

    Updated DB
    Deleted firstname, lastname and emailid from user table and added SID to
    reference survey. Created Results table to store UID, SID, QID and
    optiontext.

--- db/facebookapi_project1.sql
+++ db/facebookapi_project1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2013 at 04:01 PM
+-- Generation Time: Oct 12, 2013 at 03:43 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.1
 
@@ -32,9 +32,27 @@ CREATE TABLE IF NOT EXISTS `question` (
   `QID` int(10) NOT NULL,
   `SID` int(10) NOT NULL,
   `QuesText` text NOT NULL,
-  `Option` text NOT NULL,
+  `OptionText` text NOT NULL,
   PRIMARY KEY (`QID`),
   KEY `SID` (`SID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `RID` int(11) NOT NULL,
+  `UID` int(11) NOT NULL,
+  `SID` int(11) NOT NULL,
+  `QID` int(11) NOT NULL,
+  `OptionText` text NOT NULL,
+  PRIMARY KEY (`RID`),
+  KEY `UID` (`UID`),
+  KEY `SID` (`SID`),
+  KEY `QID` (`QID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -60,10 +78,9 @@ CREATE TABLE IF NOT EXISTS `survey` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `UID` int(10) NOT NULL,
-  `FirstName` varchar(255) NOT NULL,
-  `LastName` varchar(255) NOT NULL,
-  `EMail` varchar(255) NOT NULL,
-  PRIMARY KEY (`UID`)
+  `SID` int(11) NOT NULL,
+  PRIMARY KEY (`UID`),
+  KEY `SID` (`SID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -77,10 +94,24 @@ ALTER TABLE `question`
   ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`SID`) REFERENCES `survey` (`SID`);
 
 --
+-- Constraints for table `results`
+--
+ALTER TABLE `results`
+  ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`),
+  ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`SID`) REFERENCES `survey` (`SID`),
+  ADD CONSTRAINT `results_ibfk_3` FOREIGN KEY (`QID`) REFERENCES `question` (`QID`);
+
+--
 -- Constraints for table `survey`
 --
 ALTER TABLE `survey`
   ADD CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`SID`) REFERENCES `survey` (`SID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
